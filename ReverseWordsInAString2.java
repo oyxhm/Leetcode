53ea1a2ced69ba3b8ae1bdb5131bdf92e49c4b8d
yhfyhf <xdhfying@gmail.com>
2014-12-13 19:33:25 +0800

    ReverseWordsInAString2

--- ReverseWordsInAString2.java
+++ ReverseWordsInAString2.java
@@ -5,34 +5,34 @@ Could you do it in-place without allocating extra space?
 */
 
 class ReverseWordsInAString2 {
-	// O(n) runtime, O(1) space
+    // O(n) runtime, O(1) space
 
-	public static void reverseWords(char[] s) {
-		reverse(s, 0, s.length);
-		for (int i = 0, j = 0; i <= s.length; i++) {
-			if (i == s.length || s[i] == ' ') {
-				reverse(s, j, i);
-				j = i + 1;
-			}
-		}
-	}
+    public static void reverseWords(char[] s) {
+        reverse(s, 0, s.length);
+        for (int i = 0, j = 0; i <= s.length; i++) {
+            if (i == s.length || s[i] == ' ') {
+                reverse(s, j, i);
+                j = i + 1;
+            }
+        }
+    }
 
-	public static void reverse(char[] s, int start, int end) {
-		for (int i = 0; i < (end - start) / 2; i++) {
-			char temp = s[start + i];
-			s[start + i] = s[end - i - 1];
-			s[end - i - 1] = temp;
-		}
-	}
+    public static void reverse(char[] s, int start, int end) {
+        for (int i = 0; i < (end - start) / 2; i++) {
+            char temp = s[start + i];
+            s[start + i] = s[end - i - 1];
+            s[end - i - 1] = temp;
+        }
+    }
 
-	public static void main(String[] args) {
-		char[][] testCases = {
-			{'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'},
-			{'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!'}
-		};
-		for (char[] sentence : testCases) {
-			reverseWords(sentence);
-			System.out.println(sentence);
-		}
-	}
+    public static void main(String[] args) {
+        char[][] testCases = {
+            {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'},
+            {'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!'}
+        };
+        for (char[] sentence : testCases) {
+            reverseWords(sentence);
+            System.out.println(sentence);
+        }
+    }
 }
