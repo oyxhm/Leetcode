2ee0e43d2e32c38a4fe9a0ad2761cc878046623a
yhfyhf <xdhfying@gmail.com>
2014-12-13 19:27:33 +0800

    RotateArrayByK

--- RotateArrayByK.java
+++ RotateArrayByK.java
@@ -4,31 +4,31 @@ For instance, with k = 3, the array [0, 1, 2, 3, 4, 5, 6] is rotated to [4, 5, 6
 */
 
 class RotateArrayByK {
-	// O(nk) runtime, O(1) space
+    // O(nk) runtime, O(1) space
 
-	public static void rotateArrayByK(int[] num, int k) {
-		while (k-- > 0) {
-			rotateArrayByOne(num);
-		}
-	}
+    public static void rotateArrayByK(int[] num, int k) {
+        while (k-- > 0) {
+            rotateArrayByOne(num);
+        }
+    }
 
-	public static void rotateArrayByOne(int[] num) {
-		int temp = num[num.length - 1];
-		for (int i = num.length - 2; i >= 0; i--) {
-			num[i + 1] = num[i];
-		} 
-		num[0] = temp;
-	}
+    public static void rotateArrayByOne(int[] num) {
+        int temp = num[num.length - 1];
+        for (int i = num.length - 2; i >= 0; i--) {
+            num[i + 1] = num[i];
+        } 
+        num[0] = temp;
+    }
 
-	public static void main(String[] args) {
-		int[] testCase = {0 ,1, 2, 3, 4, 5, 6};
-		rotateArrayByK(testCase, 3);
-		for (int i = 0; i < testCase.length; i++) {
-			if (i != 0) {
-				System.out.printf(", ");
-			}
-			System.out.printf("%d", testCase[i]);	
-		}
-		System.out.printf("%n");
-	}
+    public static void main(String[] args) {
+        int[] testCase = {0 ,1, 2, 3, 4, 5, 6};
+        rotateArrayByK(testCase, 3);
+        for g(int i = 0; i < testCase.length; i++) {
+            if (i != 0) {
+                System.out.printf(", ");
+            }
+            System.out.printf("%d", testCase[i]);   
+        }
+        System.out.printf("%n");
+    }
 }
