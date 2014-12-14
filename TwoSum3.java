c0047527207ce8e7c3eb9e115f798b12074e1a13
yhf <xdhfying@gmail.com>
2014-12-14 09:48:13 +0800

    TwoSum3

--- TwoSum3.java
+++ TwoSum3.java
@@ -9,40 +9,40 @@ add(1); add(3); add(5); find(4)->true; find(7)->false
 import java.util.*;
 
 class TwoSum3 {
-	// add: O(1) runtime 
-	// find: O(n) runtime
-	// O(n) space	
+    // add: O(1) runtime 
+    // find: O(n) runtime
+    // O(n) space   
 
-	private static Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    private static Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 
-	public static void add(int input) {
-		int count = map.containsKey(input)? map.get(input) : 0;
-		map.put(input, count + 1);
-	}
+    public static void add(int input) {
+        int count = map.containsKey(input)? map.get(input) : 0;
+        map.put(input, count + 1);
+    }
 
-	public static boolean find(int value) {
-		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-			int num = entry.getKey();
-			int remainder = value - num;
-			if (remainder == num) {
-				if (entry.getValue() > 2) {
-					return true;
-				}
-			} else {
-				if (map.containsKey(value - num)) {
-					return true;
-				}
-			}
-		} // end for
-		return false;
-	}
+    public static boolean find(int value) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int num = entry.getKey();
+            int remainder = value - num;
+            if (remainder == num) {
+                if (entry.getValue() > 2) {
+                    return true;
+                }
+            } else {
+                if (map.containsKey(value - num)) {
+                    return true;
+                }
+            }
+        } // end for
+        return false;
+    }
 
-	public static void main(String[] args) {
-		TwoSum3 twosum = new TwoSum3();
-		twosum.add(1);
-		twosum.add(3);
-		twosum.add(5);
-		System.out.println(twosum.find(4));
-		System.out.println(twosum.find(7));
-	}
+    public static void main(String[] args) {
+        TwoSum3 twosum = new TwoSum3();
+        twosum.add(1);
+        twosum.add(3);
+        twosum.add(5);
+        System.out.println(twosum.find(4));
+        System.out.println(twosum.find(7));
+    }
 }
