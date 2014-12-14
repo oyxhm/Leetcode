f373a60ab34894423972de3515a8f140d4940100
yhf <yhf@MacBook-Pro.local>
2014-12-14 09:44:51 +0800

    ValidPalindrome

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -6,29 +6,29 @@ Define empty string as valid palindrome.
 */
 
 class ValidPalindrome {
-	// O(n) runtime, O(1) space
+    // O(n) runtime, O(1) space
 
-	public static boolean isValidPalindrome(String s) {
-		int i = 0, j = s.length() - 1;
-		for (;i < j; i++, j--) {
-			while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
-				i++; 
-			}
-			while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
-				j--;
-			}
-			if (Character.toLowerCase(s.charAt(i)) 
-					!= Character.toLowerCase(s.charAt(j))) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public static boolean isValidPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        for (;i < j; i++, j--) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++; 
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+            if (Character.toLowerCase(s.charAt(i)) 
+                    != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public static void main(String[] args) {
-		String[] testCases = {"abcba", "a Bcba", "a  b  cb   A", "a1 2a21a", " ", ""};
-		for (String s : testCases) {
-			System.out.printf("%s is %b%n", s, isValidPalindrome(s));	
-		}
-	}
+    public static void main(String[] args) {
+        String[] testCases = {"abcba", "a Bcba", "a  b  cb   A", "a1 2a21a", " ", ""};
+        for (String s : testCases) {
+            System.out.printf("%s is %b%n", s, isValidPalindrome(s));   
+        }
+    }
 }
