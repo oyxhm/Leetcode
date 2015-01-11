e9110bf5d7a913009c63f69b79cb48641f1a5bed
yhfyhf <yhf406716870@gmail.com>
2015-01-11 13:28:01 +0800

    ValidPalindrome

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -1,7 +1,14 @@
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+For example,
+"A man, a plan, a canal: Panama" is a palindrome. "race a car" is not a palindrome.
+Define empty string as valid palindrome.
+*/
+
 class ValidPalindrome {
 	// O(n) runtime, O(1) space
 
-	public static boolean validPalindrome(String s) {
+	public static boolean isValidPalindrome(String s) {
 		int i = 0, j = s.length() - 1;
 		for (;i < j; i++, j--) {
 			while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
@@ -21,7 +28,7 @@ class ValidPalindrome {
 	public static void main(String[] args) {
 		String[] testCases = {"abcba", "a Bcba", "a  b  cb   A", "a1 2a21a", " ", ""};
 		for (String s : testCases) {
-			System.out.printf("%s is %b%n", s, validPalindrome(s));	
+			System.out.printf("%s is %b%n", s, isValidPalindrome(s));	
 		}
 	}
 }
