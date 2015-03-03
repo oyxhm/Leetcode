2ae59f7acfe9553e2cd9bee4502b0f3bd8c22174
yhf <xdhfying@gmail.com>
2015-03-03 18:31:12 +0800

    Intersection Of Two Linked Lists

--- IntersectionOfTwoLinkedLists.java
+++ IntersectionOfTwoLinkedLists.java
@@ -12,71 +12,71 @@ begin to intersect at node c1.
 */
 
 class ListNode {
-	int val;
-	ListNode next;
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
 }
 
 public class IntersectionOfTwoLinkedLists {
-	public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-		ListNode nodeA = headA, nodeB = headB;
-		int lenA = 0, lenB = 0, diff;
-		while (nodeA != null) {
-			lenA++;
-			nodeA = nodeA.next;
-		}
-		while (nodeB != null) {
-			lenB++;
-			nodeB = nodeB.next;
-		}
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode nodeA = headA, nodeB = headB;
+        int lenA = 0, lenB = 0, diff;
+        while (nodeA != null) {
+            lenA++;
+            nodeA = nodeA.next;
+        }
+        while (nodeB != null) {
+            lenB++;
+            nodeB = nodeB.next;
+        }
 
-		nodeA = headA;
-		nodeB = headB;
-		if (lenA > lenB) {
-			diff = lenA - lenB;
-			while (diff-- != 0) {
-				nodeA = nodeA.next;
-			}
-		} else {
-			diff = lenB - lenA;
-			while (diff-- != 0) {
-				nodeB = nodeB.next;
-			}
-		}
+        nodeA = headA;
+        nodeB = headB;
+        if (lenA > lenB) {
+            diff = lenA - lenB;
+            while (diff-- != 0) {
+                nodeA = nodeA.next;
+            }
+        } else {
+            diff = lenB - lenA;
+            while (diff-- != 0) {
+                nodeB = nodeB.next;
+            }
+        }
 
-		while (nodeA != null && nodeB != null) {
-			if (nodeA == nodeB) {
-				return nodeA;
-			}
-			nodeA = nodeA.next;
-			nodeB = nodeB.next;
-		}
-		return null;
-	}
+        while (nodeA != null && nodeB != null) {
+            if (nodeA == nodeB) {
+                return nodeA;
+            }
+            nodeA = nodeA.next;
+            nodeB = nodeB.next;
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
-    	ListNode a1 = new ListNode(1);
-    	ListNode a2 = new ListNode(2);
-    	ListNode b1 = new ListNode(3);
-    	ListNode b2 = new ListNode(4);
-    	ListNode b3 = new ListNode(5);
-    	ListNode c1 = new ListNode(6);
-    	ListNode c2 = new ListNode(7);
-    	ListNode c3 = new ListNode(8);
-    	ListNode headA = a1;
-    	ListNode headB = b1;
-    	a1.next = a2;
-    	a2.next = c1;
-    	b1.next = b2;
-    	b2.next = b3;
-    	b3.next = c1;
-    	c1.next = c2;
-    	c2.next = c3;
+        ListNode a1 = new ListNode(1);
+        ListNode a2 = new ListNode(2);
+        ListNode b1 = new ListNode(3);
+        ListNode b2 = new ListNode(4);
+        ListNode b3 = new ListNode(5);
+        ListNode c1 = new ListNode(6);
+        ListNode c2 = new ListNode(7);
+        ListNode c3 = new ListNode(8);
+        ListNode headA = a1;
+        ListNode headB = b1;
+        a1.next = a2;
+        a2.next = c1;
+        b1.next = b2;
+        b2.next = b3;
+        b3.next = c1;
+        c1.next = c2;
+        c2.next = c3;
 
-    	ListNode node = getIntersectionNode(headA, headB);
-    	System.out.println(node.val);
+        ListNode node = getIntersectionNode(headA, headB);
+        System.out.println(node.val);
     }
 }
