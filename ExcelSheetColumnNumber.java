ff847539e24a491f582741e139f2db29d5687260
yhf <xdhfying@gmail.com>
2015-04-11 18:36:14 +0800

    Excel Sheet Column Number

--- ExcelSheetColumnNumber.java
+++ ExcelSheetColumnNumber.java
@@ -16,18 +16,18 @@ For example:
 */
 
 public class ExcelSheetColumnNumber {
-	public static int excelSheetColumnNumber(String s) {
-		int sum = 0;
-		for (char c : s.toCharArray()) {
-			sum = sum * 26 + c - 'A' + 1;
-		}
-		return sum;
-	}
+    public static int excelSheetColumnNumber(String s) {
+        int sum = 0;
+        for (char c : s.toCharArray()) {
+            sum = sum * 26 + c - 'A' + 1;
+        }
+        return sum;
+    }
 
-	public static void main(String[] args) {
-		System.out.println(excelSheetColumnNumber("D"));
-		System.out.println(excelSheetColumnNumber("AA"));
-		System.out.println(excelSheetColumnNumber("AB"));
-		System.out.println(excelSheetColumnNumber("ACAB"));
-	}
+    public static void main(String[] args) {
+        System.out.println(excelSheetColumnNumber("D"));
+        System.out.println(excelSheetColumnNumber("AA"));
+        System.out.println(excelSheetColumnNumber("AB"));
+        System.out.println(excelSheetColumnNumber("ACAB"));
+    }
 }
