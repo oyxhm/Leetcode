857f592490aaa95976316ef770987a9c9093ee1c
yhf <xdhfying@gmail.com>
2015-04-11 18:36:23 +0800

    Excel Sheet Column Title

--- ExcelSheetColumnTitle.java
+++ ExcelSheetColumnTitle.java
@@ -16,35 +16,35 @@ For example:
 */
 
 public class ExcelSheetColumnTitle {
-	public static String excelSheetColumnTitle(int n) {
-		int i = n, j;
-		StringBuilder sb = new StringBuilder();
-		while (i != 0) {
-			j = i % 26;
-			i = i / 26;
-			if (j == 0) {
-				/*  BE CAREFUL HERE!!!!!!! */
-				sb.append('Z');
-				i--;   
-			} else {
-				sb.append(intToChar(j));
-			}
-		}
-		
-		return sb.reverse().toString();
-	}
+    public static String excelSheetColumnTitle(int n) {
+        int i = n, j;
+        StringBuilder sb = new StringBuilder();
+        while (i != 0) {
+            j = i % 26;
+            i = i / 26;
+            if (j == 0) {
+                /*  BE CAREFUL HERE!!!!!!! */
+                sb.append('Z');
+                i--;   
+            } else {
+                sb.append(intToChar(j));
+            }
+        }
+        
+        return sb.reverse().toString();
+    }
 
-	public static char intToChar(int n) {
-		char c = (char) ((int)'A' + n - 1);
-		return c;
-	}
+    public static char intToChar(int n) {
+        char c = (char) ((int)'A' + n - 1);
+        return c;
+    }
 
-	public static void main(String[] args) {
-		System.out.println(excelSheetColumnTitle(3));
-		System.out.println(excelSheetColumnTitle(26));
-		System.out.println(excelSheetColumnTitle(28));
-		System.out.println(excelSheetColumnTitle(52));
-		System.out.println(excelSheetColumnTitle(18280));
-		System.out.println(excelSheetColumnTitle(19632));
-	}
+    public static void main(String[] args) {
+        System.out.println(excelSheetColumnTitle(3));
+        System.out.println(excelSheetColumnTitle(26));
+        System.out.println(excelSheetColumnTitle(28));
+        System.out.println(excelSheetColumnTitle(52));
+        System.out.println(excelSheetColumnTitle(18280));
+        System.out.println(excelSheetColumnTitle(19632));
+    }
 }
