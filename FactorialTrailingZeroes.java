847bc2d967ffd08a92d4c6aca3e0e0b8c0e1f25d
yhf <xdhfying@gmail.com>
2015-04-11 18:36:58 +0800

    Factorial Trailing Zeroes

--- FactorialTrailingZeroes.java
+++ FactorialTrailingZeroes.java
@@ -2,7 +2,7 @@
 * @Author: yhf
 * @Date:   2015-04-11 10:44:58
 * @Last Modified by:   yhf
-* @Last Modified time: 2015-04-11 11:13:43
+* @Last Modified time: 2015-04-11 18:36:40
 */
 
 /*
@@ -14,28 +14,28 @@ Note: Your solution should be in logarithmic time complexity.
 import java.util.*;
 
 public class FactorialTrailingZeroes {
-	public static int factorialTrailingZeroes(int n) {
-		List<Integer> l = new ArrayList<Integer>();
-		int i = n, j;
-		while (i > 0) {
-			j = i % 5;
-			i = i / 5;
-			l.add(j);
-		}
-		int sum = 0, count = 0;
-		for (int num : l) {
-			sum += count * num;
-			count = count * 5 + 1;
-		}
-		return sum;
-	}
+    public static int factorialTrailingZeroes(int n) {
+        List<Integer> l = new ArrayList<Integer>();
+        int i = n, j;
+        while (i > 0) {
+            j = i % 5;
+            i = i / 5;
+            l.add(j);
+        }
+        int sum = 0, count = 0;
+        for (int num : l) {
+            sum += count * num;
+            count = count * 5 + 1;
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
-    	System.out.println(factorialTrailingZeroes(4));
-    	System.out.println(factorialTrailingZeroes(5));
-    	System.out.println(factorialTrailingZeroes(6));
-    	System.out.println(factorialTrailingZeroes(25));
-    	System.out.println(factorialTrailingZeroes(24));
-    	System.out.println(factorialTrailingZeroes(69));
+        System.out.println(factorialTrailingZeroes(4));
+        System.out.println(factorialTrailingZeroes(5));
+        System.out.println(factorialTrailingZeroes(6));
+        System.out.println(factorialTrailingZeroes(25));
+        System.out.println(factorialTrailingZeroes(24));
+        System.out.println(factorialTrailingZeroes(69));
     }
 }
