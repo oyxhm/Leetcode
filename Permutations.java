b91b6ae96f628239726beea7b3d377297dc65a3a
yhf <xdhfying@gmail.com>
2015-05-26 15:24:24 +0800

    Permutations, add BFS solution

--- Permutations.java
+++ Permutations.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by yhf on 15/5/22.
@@ -47,7 +45,36 @@ public class Permutations {
         nums[j] = temp;
     }
 
+    public static List<List<Integer>> permute2(int[] nums) {
+        // bfs
+        // 先在队列中插入nums[0], 之后每个num分别插入队列中每个list的每个位置
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<List<Integer>> cur = new LinkedList<>();
+        if (nums.length == 0) {
+            return res;
+        }
+        List<Integer> l = new ArrayList<>();
+        l.add(nums[0]);
+        cur.add(l);
+        for (int i = 1; i < nums.length; i++) {
+            int size = cur.size();
+            for (int j = 0; j < size; j++) {
+                List<Integer> temp = cur.remove();
+                for (int idx = 0; idx <= temp.size(); idx++) {
+                    List<Integer> newTemp = new ArrayList<>(temp);
+                    newTemp.add(idx, nums[i]);
+                    cur.add(newTemp);
+                }
+            }
+        }
+        while (!cur.isEmpty()) {
+            List<Integer> temp = cur.remove();
+            res.add(temp);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        List<List<Integer>> res = permute(new int[]{1, 2, 3});
+        List<List<Integer>> res = permute2(new int[]{1, 2, 3});
     }
 }
